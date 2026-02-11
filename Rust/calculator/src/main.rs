2b91d077a79f9191ff51b4662179b9bf365bec4c
bxrisyvnev <borisyanev0000@gmail.com>
2026-02-12 00:33:40 +0100

    feat/rust: add while loop in calculator

--- Rust/calculator/src/main.rs
+++ Rust/calculator/src/main.rs
@@ -7,43 +7,45 @@ use operators::sub;
 use std::io;
 
 fn main() {
-    let mut x = String::new();
+    while true {
+        let mut x = String::new();
 
-    io::stdin()
-        .read_line(&mut x)
-        .expect("Pls type an actual number pls!");
+        io::stdin()
+            .read_line(&mut x)
+            .expect("Pls type an actual number pls!");
 
-    let x: i32 = x.trim().parse().expect("Please type a number!");
+        let x: i32 = x.trim().parse().expect("Please type a number!");
 
-    let mut operator = String::new();
+        let mut operator = String::new();
 
-    io::stdin()
-        .read_line(&mut operator)
-        .expect("Please type an operator");
+        io::stdin()
+            .read_line(&mut operator)
+            .expect("Please type an operator");
 
-    let operator = operator.trim();
+        let operator = operator.trim();
 
-    let mut y = String::new();
+        let mut y = String::new();
 
-    io::stdin()
-        .read_line(&mut y)
-        .expect("Pls type an actual number pls!");
+        io::stdin()
+            .read_line(&mut y)
+            .expect("Pls type an actual number pls!");
 
-    let y: i32 = y.trim().parse().expect("Please type a number!");
+        let y: i32 = y.trim().parse().expect("Please type a number!");
 
-    if operator == "+" {
-        let result: i32 = add::add(x, y);
-        println!("Answer is {result}");
-    } else if operator == "-" {
-        let result: i32 = sub::sub(x, y);
-        println!("Answer is {result}");
-    } else if operator == "/" {
-        let result: i32 = div::div(x, y);
-        println!("Answer is {result}");
-    } else if operator == "*" {
-        let result: i32 = mul::mul(x, y);
-        println!("Answer is {result}");
-    } else {
-        println!("Invalid operator!")
+        if operator == "+" {
+            let result: i32 = add::add(x, y);
+            println!("Answer is {result}");
+        } else if operator == "-" {
+            let result: i32 = sub::sub(x, y);
+            println!("Answer is {result}");
+        } else if operator == "/" {
+            let result: i32 = div::div(x, y);
+            println!("Answer is {result}");
+        } else if operator == "*" {
+            let result: i32 = mul::mul(x, y);
+            println!("Answer is {result}");
+        } else {
+            println!("Invalid operator!")
+        }
     }
 }
